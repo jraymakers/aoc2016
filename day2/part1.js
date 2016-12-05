d161e867ed43d4aed7cb940d90ced1e55bb9e476
Andrew Brandon <drbr@users.noreply.github.com>
2016-12-04 21:58:53 -0800

    Hopefully fixed it, and added another example

--- day2/part1.js
+++ day2/part1.js
@@ -1,19 +1,29 @@
 const fs = require('fs');
 
+const buttons = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
 const dirMovements = {
-  U: -3,
-  D: 3,
-  L: -1,
-  R: 1
+  U: [0, -1],
+  D: [0, 1],
+  L: [-1, 0],
+  R: [1, 0]
 };
 
 function move(pos, dir) {
-  let newPos = pos + dirMovements[dir];
+  let [x, y] = pos;
+  let [moveX, moveY] = dirMovements[dir];
+  let newPos = [x + moveX, y + moveY];
   return inRange(newPos) ? newPos : pos;
 }
 
 function inRange(pos) {
-  return pos >= 1 && pos <= 9;
+  let [x, y] = pos;
+  return x >= 0 && x < buttons[0].length &&
+    y >= 0 && y < buttons.length;
 }
 
 let data = fs.readFileSync(process.argv[2], 'utf8');
@@ -28,6 +38,6 @@ function lineToButton(buttons, line) {
   return buttons.concat(pos);
 }
 
-let buttons = lines.reduce(lineToButton, [5]);
-buttons = buttons.slice(1);
+let buttonSequence = lines.reduce(lineToButton, [1, 1]);
+buttonSequence = buttonSequence.slice(1);
 console.log(buttons.join(''));
