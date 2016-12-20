62ed630d91f6a7b65043da81a7a1a8a7f5024e9f
Andrew Brandon <drbr@users.noreply.github.com>
2016-12-19 20:59:05 -0800

    Day 17 Part 1 Andrew

---
+++ day17/part1Andrew.js
@@ -0,0 +1,67 @@
+const crypto = require('crypto');
+
+function getMD5(text) {
+  const hash = crypto.createHash('md5');
+  hash.update(text);
+  return hash.digest('hex');
+}
+
+const openLetters = 'bcdef';
+function isDoorOpen(doorChar) {
+  return openLetters.includes(doorChar);
+}
+
+function getDoorStatuses(passcode, x, y) {
+  let hash = getMD5(passcode).slice(0, 4);
+  return {
+    U: isDoorOpen(hash[0]),
+    D: isDoorOpen(hash[1]),
+    L: isDoorOpen(hash[2]),
+    R: isDoorOpen(hash[3])
+  }
+}
+
+const directionMovements = {
+  U: [0, -1],
+  D: [0, 1],
+  L: [-1, 0],
+  R: [1, 0]
+}
+
+function roomInBounds(x, y) {
+  return x >= 0 && x <= 3 && y >= 0 && y <= 3;
+}
+
+function doSearch(q) {
+  while (q.length > 0) {
+    let { x, y, sequence } = q.shift();
+
+    if (x === 3 && y === 3) {
+      return sequence;
+    }
+
+    let doorStatuses = getDoorStatuses(sequence, x, y);
+    for (direction in doorStatuses) {
+      if (!doorStatuses[direction]) {
+        continue;
+      }
+
+      let movement = directionMovements[direction];
+      let newX = x + movement[0];
+      let newY = y + movement[1];
+      let newSequence = sequence + direction;
+      if (roomInBounds(newX, newY)) {
+        q.push({ x: newX, y: newY, sequence: newSequence });
+      }
+    }
+  }
+}
+
+function run(passcode) {
+  let q = [ { x: 0, y: 0, sequence: passcode } ];
+  let path = doSearch(q);
+  console.log(path.slice(passcode.length));
+}
+
+run('awrkjxxr');
+
